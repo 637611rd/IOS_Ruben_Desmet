ac6a28c3a3738621dca020bbf6c1f98a8bc1e7cc
637611rd <ruben.desmet.y7611@student.hogent.be>
2018-12-11 20:01:05 +0100

    Probleem bij doorklikken categoriën opgelost

--- frituur/frituur/View Controllers/CategorieViewController.swift
+++ frituur/frituur/View Controllers/CategorieViewController.swift
@@ -5,7 +5,7 @@ import UIKit
 class CategorieViewController: UITableViewController {
     
     var categorieSelected:Categorie=Categorie(naam: "", beschrijving: "")
-    var magDoorOfNiet: Bool=false
+   
     
     var categories: [Categorie] = [
         Categorie(naam: "Snacks", beschrijving: "Frikandel, Boulet, ..."),
@@ -46,9 +46,9 @@ class CategorieViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt
         indexPath: IndexPath) {
-        self.categorieSelected = categories[indexPath.row]
-        magDoorOfNiet=true
-        print("\(categorieSelected.naam) \(indexPath)")
+        //self.categorieSelected = categories[indexPath.row]
+        
+       // print("\(categorieSelected.naam) \(indexPath)")
     }
     // MARK: - Navigation
     
@@ -58,7 +58,8 @@ class CategorieViewController: UITableViewController {
         // Pass the selected object to the new view controller.
      // let des = segue.destination as! UITableViewController
       //  des.categorie=self.categorieSelected
-        
+        let indexPath = tableView.indexPathForSelectedRow!
+         categorieSelected = categories[indexPath.row]
         let tableViewController = segue.destination as! UITableViewController
         let itemTableViewController = tableViewController as! ItemTableViewController
         itemTableViewController.categorie=self.categorieSelected
